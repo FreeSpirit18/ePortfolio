95d5ffdc3b93eafb7ea8ed5cbfa0b164b4638f85
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-11-23 12:57:08 +0200

    Login completed

--- Website/eportfolio/src/components/LogIn/Form.js
+++ Website/eportfolio/src/components/LogIn/Form.js
@@ -1,31 +1,42 @@
 import { useState } from 'react'
 import '../../styles/Login.css'
 import axios from 'axios'
+import {useNavigate } from 'react-router-dom';
 
 function Form(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [token, setToken] = useState('');
+    const [loginFailed, setLoginFailed] = useState(false);
 
+    const nav = useNavigate();
     const api = process.env.REACT_APP_API + 'User/login';
 
     const submit = async () =>{
-        
-        const handleLogin = await axios.post(api, {
-            email: email,
-            password: password
-        });
-        
-        setToken(handleLogin.data.accessToken);
+        try{
+
+            const handleLogin = await axios.post(api, {
+                email: email,
+                password: password
+            });
+            
+            setToken(handleLogin.data.accessToken);
+            localStorage.setItem('AuthToken',token);
+            nav("/");
+        }catch(error){
+            setPassword("");
+            setLoginFailed(true);
+        }
     }
 
     return(
         <>
             <form className='login-form'>
-                <p>Email:</p>
+                <p className='default'>Email:</p>
                 <input className='text-imput' type='text' value={email}  onChange={ev => setEmail(ev.target.value)} required/><br/>
-                <p>Password:</p>
+                <p className='default'>Password:</p>
                 <input className='text-imput' type='text' value={password}  onChange={ev => setPassword(ev.target.value)} required/><br/>
+                {loginFailed && <p className="error-text">Login failed. The email or the password was incorect</p>}
                 <input
                 className="input-Button"
                 type="button"
--- Website/eportfolio/src/components/TaskBar.js
+++ Website/eportfolio/src/components/TaskBar.js
@@ -2,12 +2,21 @@ import '../styles/TaskBar.css'
 import { Link } from 'react-router-dom';
 
 function TaskBar(){
+    
     return(
         <>
         <div className="box">
             <div className="group">
                 <div className="overlap-group">
                     <text className="text-wrapper">ePortfolio</text>
+                    
+                    <div className="div-wrapper">
+                        <div className="group-3">
+                        <img className="frame-3" alt="Frame" src={process.env.PUBLIC_URL + '/magnify.svg'} />
+                        <div className="text-wrapper-3">Search works</div>
+                        </div>
+                    </div>
+
                     <div className="div">
                         <img className="frame" alt="Frame" src={process.env.PUBLIC_URL + '/bell.svg'} />
                         <div className="group-wrapper">
@@ -18,12 +27,6 @@ function TaskBar(){
                         </div>
                         <div className="frame-wrapper">
                         <img className="frame-2" alt="Frame" src={process.env.PUBLIC_URL + '/account.svg'} />
-                        </div>
-                    </div>
-                    <div className="div-wrapper">
-                        <div className="group-3">
-                        <img className="frame-3" alt="Frame" src={process.env.PUBLIC_URL + '/magnify.svg'} />
-                        <div className="text-wrapper-3">Search works</div>
                         </div>
                     </div>
                 </div>
