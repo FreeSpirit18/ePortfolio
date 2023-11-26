b903a1c9a1a678093ae7c49f09af28d71167d964
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-11-26 19:41:56 +0200

    Can display grid of images

--- Website/eportfolio/src/pages/Home.js
+++ Website/eportfolio/src/pages/Home.js
@@ -11,15 +11,14 @@ import { useState } from "react";
     const [posts, setPosts] = useState([]);
   
     useEffect(() => {
-        // Fetch posts from your API
         const handlePost = axios.get(api+'Post');
         handlePost.then(Response =>{
 
             setPosts(Response.data)
-            console.log(posts[0].location);
+            console.log(posts[0]);
         })
       }, []);
-    //const Post = await axios.get(api+'Post');
+
     const heart = process.env.PUBLIC_URL + '/heart-outline.svg'
     return(
         <>
@@ -27,65 +26,72 @@ import { useState } from "react";
             <div className="home">
                 
                 <div className="div">
-                    
-                    <div className="group-5">
-                    <div className="group-6">
-                        <img className="img-wrapper" src="https://eportfoliostore.blob.core.windows.net/pictures/638365236654614390_4f40c61c-5adf-4327-98fa-c1e4dc24a0e0.jpg">
-                            
-                        </img>
-                        <div className="group-7">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-8">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-9">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-10">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                    </div>
-                    <div className="group-11">
-                        <div className="img-wrapper">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-7">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-8">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-9">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-10">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                    </div>
-                    <div className="group-12">
-                        <div className="img-wrapper">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-7">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-8">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-9">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                        <div className="group-10">
-                        <img className="frame-4" alt="Frame" src={heart} />
-                        </div>
-                    </div>
-                    </div>
                     <div className="overlap">
-                    <div className="text-wrapper-4">Popular</div>
-                    <div className="rectangle" />
-                    <div className="text-wrapper-5">Latest</div>
+                        <div className="text-wrapper-4">Popular</div>
+                        <div className="rectangle" />
+                        <div className="text-wrapper-5">Latest</div>
                     </div>
+                    <div className="grid-container">
+                        {posts.map((post) => (
+                            <div key={post.id} className="grid-item">
+                                <img src={post.location} alt={post.name} />
+                                <p>{post.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                    {/* <div className="group-5">
+                        <div className="group-6">
+                            <img className="img-wrapper" src="https://eportfoliostore.blob.core.windows.net/pictures/638365236654614390_4f40c61c-5adf-4327-98fa-c1e4dc24a0e0.jpg">
+                                
+                            </img>
+                            <div className="group-7">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-8">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-9">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-10">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                        </div>
+                        <div className="group-11">
+                            <div className="img-wrapper">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-7">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-8">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-9">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-10">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                        </div>
+                        <div className="group-12">
+                            <div className="img-wrapper">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-7">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-8">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-9">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                            <div className="group-10">
+                            <img className="frame-4" alt="Frame" src={heart} />
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </>
